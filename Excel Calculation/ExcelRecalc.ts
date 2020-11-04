aaa2f32e994f1a2c5317401582231d1b016968f3
Sudhi Ramamurthy <suramam@microsoft.com>
2020-11-04 13:44:15 -0800

    Update ExcelRecalc.ts

--- Excel Calculation/ExcelRecalc.ts
+++ Excel Calculation/ExcelRecalc.ts
@@ -1,16 +1,9 @@
-# Excel calculation and setting calculation mode 
-
-This project shows how to use Excel application's calculation mode and calculate methods in Excel for web using Office Scripts. 
-
-## Video 
-Checkout the video at: https://youtu.be/iw6O8QH01CI
-
-## Scenario
-
-Excel for web file's calculation mode can be controlled programatically using simple APIs. Following actions are possible using Office Scripts:
-
-1. Get calculation mode (automatic or manual, etc.) 
-1. Set the calculation mode 
-1. Calculate Exel formulas for files that are set to the manual mode (also referred as re-calculate) 
-
-Checkout the script and video for more details. 
+function main(workbook: ExcelScript.Workbook) {
+    // Set calculation mode
+    workbook.getApplication().setCalculationMode(ExcelScript.CalculationMode.manual);
+    // Get calculation mode 
+    const calcMode = workbook.getApplication().getCalculationMode();    
+    console.log(calcMode);
+    // Calculate (for manual mode files)
+    workbook.getApplication().calculate(ExcelScript.CalculationType.full);
+}
