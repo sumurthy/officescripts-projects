129cf645752682c4d98bb9ef15f2ab64c1aed068
Sudhi Ramamurthy <suramam@microsoft.com>
2020-11-06 11:46:50 -0800

    Create RemoveHyperlink.ts

---
+++ Remove Hyperlinks from Excel Cells/RemoveHyperlink.ts
@@ -0,0 +1,60 @@
+/**
+ * This sample clears all of the hyperlink from the current worksheet. 
+ * It traverses through the worksheet and if there is any hyperlink associated with the cell, it cleares the hyperlink and retains the cell value as is. 
+ */
+function main(workbook: ExcelScript.Workbook) {
+
+  // Get the active worksheet. 
+  let sheet = workbook.getActiveWorksheet();
+  const targetRange = sheet.getUsedRange(true);
+  if (!targetRange) {
+    console.log(`There is no data in the worksheet. `)
+    return;
+  }
+  console.log(`Target Range to clear hyperlinks from: ${targetRange.getAddress()}`);
+
+  const rowCount = targetRange.getRowCount();
+  const colCount = targetRange.getColumnCount();
+  const totalCells = rowCount * colCount;
+  if (totalCells > 10000) {
+    console.log("Too many cells to operate with. Consider editing script to use selected range and then remove hyperlinks in batches. " + targetRange.getAddress());
+    return;
+  }
+  // Call the helper function to remove the hyperlinks. 
+  removeHyperLink(targetRange);
+  return;
+}
+
+/**
+ * Removes hyperlink for each cell in the target range. Logs the time it takes to complete traversal.
+ * @param targetRange Target range to clear the hyperlinks from.
+ */
+function removeHyperLink(targetRange: ExcelScript.Range): void {
+  const rowCount = targetRange.getRowCount();
+  const colCount = targetRange.getColumnCount();
+  console.log(`Searching for hyperliinks in ${targetRange.getAddress()} which contains ${(rowCount * colCount)} cells`);
+  let clearedCount = 0;
+  let cellsVisited = 0;
+
+  let groupStart = new Date().getTime();
+  for (let i = 0; i < rowCount; i++) {
+    for (let j = 0; j < colCount; j++) {
+      cellsVisited++;
+      if (cellsVisited % 50 === 0) {
+        let groupEnd = new Date().getTime();
+        console.log(`Completed ${cellsVisited} cells out of ${rowCount * colCount}. This group took: ${(groupEnd - groupStart) / 1000} seconds to complete.`);
+        groupStart = new Date().getTime();
+      }
+      const cell = targetRange.getCell(i, j);
+      const hyperlink = cell.getHyperlink();
+      if (hyperlink) {
+        cell.clear(ExcelScript.ClearApplyTo.hyperlinks);
+        cell.getFormat().getFont().setUnderline(ExcelScript.RangeUnderlineStyle.none);
+        cell.getFormat().getFont().setColor('Black');
+        clearedCount++;
+      }
+    }
+  }
+  console.log(`Done. Inspected ${cellsVisited} cells. Clearned hyperlinks in: ${clearedCount} cells`);
+  return;
+}
