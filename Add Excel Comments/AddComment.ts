958181895d9dc680e2955435c0068caf616cd687
Sudhi Ramamurthy <suramam@microsoft.com>
2020-11-06 14:45:22 -0800

    Create AddComment.ts

---
+++ Add Excel Comments/AddComment.ts
@@ -0,0 +1,37 @@
+function main(workbook: ExcelScript.Workbook) {
+    const employees = workbook.getWorksheet('Employees').getUsedRange().getTexts();
+    console.log(employees); 
+
+    const scheduleSheet = workbook.getWorksheet('Schedule');
+    const table = scheduleSheet.getTables()[0];
+    const range = table.getRangeBetweenHeaderAndTotal();
+    const scheduleData = range.getTexts();
+
+    for (let i=0; i < scheduleData.length; i++) {
+      let eId = scheduleData[i][3];
+
+      let employeeInfo = employees.find(e => e[0] === eId);
+      if (employeeInfo) {
+        console.log("Found a match " + employeeInfo);
+        let adminNotes = scheduleData[i][4];
+        try { 
+          let comment = workbook.getCommentByCell(range.getCell(i, 5));
+          comment.delete();
+        } catch {
+            console.log("Ignore if there is no existing comment in the cell");
+        }
+        workbook.addComment(range.getCell(i,5), {
+          mentions: [{
+            email: employeeInfo[1],
+            id: 0,
+            name: employeeInfo[2]
+          }],
+          richContent: `<at id=\"0\">${employeeInfo[2]}</at> ${adminNotes}`
+        }, ExcelScript.ContentType.mention);        
+        
+      } else {
+        console.log("No match for: " + eId);
+      }
+    }
+    return;
+}
