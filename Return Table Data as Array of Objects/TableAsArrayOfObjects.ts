38007bdcf234baa24044832ddcf79fc54756e1dd
Sudhi Ramamurthy <suramam@microsoft.com>
2020-11-17 19:28:15 -0800

    Create TableAsArrayOfObjects.ts

---
+++ Return Table Data as Array of Objects/TableAsArrayOfObjects.ts
@@ -0,0 +1,41 @@
+function main(workbook: ExcelScript.Workbook): TableData[] {
+  const table = workbook.getWorksheet('Sheet3').getTables()[0];
+  // If you know the table name, you can also do this...
+  // const table = workbook.getTable('Table13436');
+  const texts = table.getRange().getTexts();
+  let returnObjects: TableData[] = [];
+  if (table.getRowCount() > 0)  {
+    returnObjects = returnObjectFromValues(texts);
+  } 
+  console.log(returnObjects);  
+  return returnObjects
+}
+
+function returnObjectFromValues(values: string[][]): TableData[] {
+  let objArray = [];
+  let objKeys: string[] = [];
+  for (let i = 0; i < values.length; i++) {
+    if (i === 0) {
+      objKeys = values[i]
+      continue;
+    }
+    let obj = {}
+    for (let j = 0; j < values[i].length; j++) {
+      obj[objKeys[j]] = values[i][j]
+    }
+    objArray.push(obj);
+  }
+  return objArray as TableData[];
+}
+
+interface BasicObj {
+  [key: string]: string
+}
+
+interface TableData {
+  "Event ID": string
+  Date: string
+  Location: string
+  Cpacity: string
+  Speakers: string
+}
