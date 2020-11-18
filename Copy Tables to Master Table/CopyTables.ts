bab4ad6d4d4b4866ee9eca6bb1b2d5ccd9c0d10e
Sudhi Ramamurthy <suramam@microsoft.com>
2020-11-17 17:28:01 -0800

    Create CopyTables.ts

---
+++ Copy Tables to Master Table/CopyTables.ts
@@ -0,0 +1,24 @@
+function main(workbook: ExcelScript.Workbook) {
+    const sheetNames = ['Sheet1', 'Sheet2', 'Sheet3'];
+    
+    workbook.getWorksheet('Combined')?.delete();
+    const newSheet = workbook.addWorksheet('Combined');
+    
+    const tables = workbook.getTables();    
+    const headerValues = tables[0].getHeaderRowRange().getTexts();
+    console.log(headerValues);
+    const targetRange = updateRange(newSheet, headerValues);
+    const combinedTable = newSheet.addTable(targetRange.getAddress(), true);
+    for (let table of tables) {
+      
+      let dataValues = table.getRangeBetweenHeaderAndTotal().getTexts();
+      combinedTable.addRows(-1, dataValues);
+    }
+}
+
+function updateRange(sheet: ExcelScript.Worksheet, data: string[][]): ExcelScript.Range {
+  const targetRange = sheet.getRange('A1').getResizedRange(data.length-1, data[0].length-1);
+  targetRange.setValues(data);
+  return targetRange;
+}
+
