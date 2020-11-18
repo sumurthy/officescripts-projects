3b7df3d05673b98460587a550792a11ae8a378c7
Sudhi Ramamurthy <suramam@microsoft.com>
2020-11-17 17:28:25 -0800

    Create CopyTablesFromSelectSheets.ts

---
+++ Copy Tables to Master Table/CopyTablesFromSelectSheets.ts
@@ -0,0 +1,27 @@
+function main(workbook: ExcelScript.Workbook) {
+    const sheetNames = ['Sheet1', 'Sheet2', 'Sheet3'];
+    
+    workbook.getWorksheet('Combined')?.delete();
+    const newSheet = workbook.addWorksheet('Combined');
+    let targetTableCreated = false;
+    let combinedTable;
+    sheetNames.forEach((sheet) => {
+      const tables = workbook.getWorksheet(sheet).getTables();
+      if (!targetTableCreated) {
+        const headerValues = tables[0].getHeaderRowRange().getTexts();
+        const targetRange = updateRange(newSheet, headerValues);
+        combinedTable = newSheet.addTable(targetRange.getAddress(), true);
+        targetTableCreated = true;
+      }      
+      for (let table of tables) {
+        let dataValues = table.getRangeBetweenHeaderAndTotal().getTexts();
+        combinedTable.addRows(-1, dataValues);
+      }
+    })
+}
+
+function updateRange(sheet: ExcelScript.Worksheet, data: string[][]): ExcelScript.Range {
+  const targetRange = sheet.getRange('A1').getResizedRange(data.length-1, data[0].length-1);
+  targetRange.setValues(data);
+  return targetRange;
+}
