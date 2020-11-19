bcc04acd7abcf1763082bca362e89122aea85bd9
Sudhi Ramamurthy <suramam@microsoft.com>
2020-11-19 10:19:36 -0800

    Create TableAsArrayOfObjectsWithHyperlink.ts

---
+++ Return Table Data as Array of Objects/TableAsArrayOfObjectsWithHyperlink.ts
@@ -0,0 +1,48 @@
+function main(workbook: ExcelScript.Workbook): TableData[] {
+  const table = workbook.getWorksheet('Sheet3').getTables()[0];
+  const range = table.getRange();
+  // If you know the table name, you can also do this...
+  // const table = workbook.getTable('Table13436');
+  const texts = table.getRange().getTexts();
+  let returnObjects: TableData[] = [];
+  if (table.getRowCount() > 0)  {
+    returnObjects = returnObjectFromValues(texts, range);
+  } 
+  console.log(JSON.stringify(returnObjects));  
+  return returnObjects
+}
+
+function returnObjectFromValues(values: string[][], range: ExcelScript.Range): TableData[] {
+  let objArray = [];
+  let objKeys: string[] = [];
+  for (let i = 0; i < values.length; i++) {
+    if (i === 0) {
+      objKeys = values[i]
+      continue;
+    }
+    let obj = {}
+    for (let j = 0; j < values[i].length; j++) {
+      // For the 4th column (0 index), extract the hyperlink and use that instead of text. 
+      if (j === 4) {
+        obj[objKeys[j]] = range.getCell(i, j).getHyperlink().address;
+      } else {
+        obj[objKeys[j]] = values[i][j];
+      }
+    }
+    objArray.push(obj);
+  }
+  return objArray as TableData[];
+}
+
+interface BasicObj {
+  [key: string]: string
+}
+
+interface TableData {
+  "Event ID": string
+  Date: string
+  Location: string
+  Capacity: string
+  "Search link": string
+  Speakers: string
+}
