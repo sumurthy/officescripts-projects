5f97707690b7bca91e3f782e21eedfc44a153e4c
Sudhi Ramamurthy <suramam@microsoft.com>
2020-12-29 15:18:43 -0800

    Create ReadWriteBasics.ts

---
+++ Range Basics/ReadWriteBasics.ts
@@ -0,0 +1,40 @@
+/**
+ * This script demonstrates basic read-write operations on the range object. 
+ */
+function main(workbook: ExcelScript.Workbook) {
+  const cell = workbook.getActiveCell();
+  const prevValue = cell.getValue();
+  if (prevValue) {
+      console.log(`Active cell's value is: ${prevValue}`);
+  } else {
+      console.log("Setting active cell's value..");
+      cell.setValue("Sample");
+  }
+  // Get next cell to the right column and set its value and fill color. 
+  const nextCell = cell.getOffsetRange(0,1);
+  nextCell.setValue("Next cell");
+  console.log(`Next cell's address is: ${nextCell.getAddress()}`);
+  console.log("Setting fill color and font color of next cell..");
+  nextCell.getFormat().getFill().setColor("Magenta");
+  nextCell.getFormat().getFill().setColor("Cyan");
+
+  // Get the target range address to update with 2-dimensional value.
+  const dataRange = nextCell.getOffsetRange(1, 0).getResizedRange(2, 1);
+  const DATA = [
+    [10, 7],
+    [8, 15],
+    [12, 1]
+  ];
+  console.log(`Updating range ${dataRange.getAddress()} with values: ${DATA}`);
+  dataRange.setValues(DATA);
+
+  // Formula range
+  const formulaRange = dataRange.getOffsetRange(3, 0).getRow(0);
+  console.log(`Updating formula for range: ${formulaRange.getAddress()}`)
+  // Since relative formula is being set, we can set the formula of the entire range to same value. 
+  formulaRange.setFormulaR1C1("=SUM(R[-3]C:R[-1]C)");
+  console.log(`Updating Number Format for range: ${formulaRange.getAddress()}`)
+  // Since the number format is common to the entire range, we can set it to a common format. 
+  formulaRange.setNumberFormat("0.00");
+  return;
+} 
