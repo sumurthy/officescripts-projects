f703697514eb44fef1582a43601386ddbe37c365
Sudhi Ramamurthy <suramam@microsoft.com>
2020-12-30 13:22:28 -0800

    Create Update2DRangeValues.ts

---
+++ Range Basics/Update2DRangeValues.ts
@@ -0,0 +1,31 @@
+function main(workbook: ExcelScript.Workbook) {
+  const currentCell = workbook.getActiveCell();
+  let inputRange = computeTargetRange(currentCell, DATA);
+  // Set range values.
+  console.log(inputRange.getAddress());
+  inputRange.setValues(DATA);
+  // Call a helper function to place border around the range. 
+  borderAround(inputRange);
+}
+/**
+ * A Helper function that computes the target range given the target range's starting cell and selected range. 
+ */
+function computeTargetRange(targetCell: ExcelScript.Range, data: string[][]): ExcelScript.Range {
+  const targetRange = targetCell.getResizedRange(data[0].length - 1, data[0][0].length - 1);
+  return targetRange;
+}
+/**
+ * A Helper function that places a border around the range.
+ */
+function borderAround(range: ExcelScript.Range): void {
+  range.getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeLeft).setStyle(ExcelScript.BorderLineStyle.dash);
+  range.getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeRight).setStyle(ExcelScript.BorderLineStyle.dash);
+  range.getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeTop).setStyle(ExcelScript.BorderLineStyle.dash);
+  range.getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeBottom).setStyle(ExcelScript.BorderLineStyle.dash);
+  return;
+}
+// Values used for range setup. 
+const DATA = [
+  ['Item', 'Bread', 'Donuts', 'Cookies', 'Cakes', 'Pies'],
+  ['Amount', '2', '1.5', '4', '12', '26']
+]
