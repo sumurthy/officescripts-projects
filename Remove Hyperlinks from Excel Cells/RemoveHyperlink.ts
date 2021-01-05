e30bb4863d437558b98666803c9cce59e6a85f0a
Sudhi Ramamurthy <suramam@microsoft.com>
2021-01-05 13:05:31 -0800

    Update RemoveHyperlink.ts

--- Remove Hyperlinks from Excel Cells/RemoveHyperlink.ts
+++ Remove Hyperlinks from Excel Cells/RemoveHyperlink.ts
@@ -1,125 +1,60 @@
-function main(workbook: ExcelScript.Workbook) {
-  const sheet = workbook.getActiveWorksheet();
+/**
+ * This sample clears all of the hyperlink from the current worksheet. 
+ * It traverses through the worksheet and if there is any hyperlink associated with the cell, it cleares the hyperlink and retains the cell value as is. 
+ */
+function main(workbook: ExcelScript.Workbook, sheetName: string = 'Sheet1') {
 
-  let data: (string | number | boolean)[][] = [];
-  // Number of rows in the random data (x 6 columns)
-  const sampleRows = 10000;
-
-  console.log(`Generating data...`)
-  // Dynamically generate some random data for testing purpose. 
-  for (let i = 0; i < sampleRows; i++) {
-    data.push([i, ...[getRandomString(5), getRandomString(20), getRandomString(10), Math.random()], "Sample data"]);
+  // Get the active worksheet. 
+  let sheet = workbook.getWorksheet(sheetName);
+  const targetRange = sheet.getUsedRange(true);
+  if (!targetRange) {
+    console.log(`There is no data in the worksheet. `)
+    return;
   }
+  console.log(`Target Range to clear hyperlinks from: ${targetRange.getAddress()}`);
 
-  console.log(`Calling update range function...`);
-  const updated = updateRangeInChunks(sheet.getRange("B2"), data);
-  if (!updated) {
-    console.log(`Update did not take place or complete. Chech and run again.`)
+  const rowCount = targetRange.getRowCount();
+  const colCount = targetRange.getColumnCount();
+  const totalCells = rowCount * colCount;
+  if (totalCells > 10000) {
+    console.log("Too many cells to operate with. Consider editing script to use selected range and then remove hyperlinks in batches. " + targetRange.getAddress());
+    return;
   }
-
+  // Call the helper function to remove the hyperlinks. 
+  removeHyperLink(targetRange);
   return;
 }
 
-function updateRangeInChunks(
-  startCell: ExcelScript.Range,
-  values: (string | boolean | number)[][],
-  cellsInChunk: number = 10000
-): boolean {
+/**
+ * Removes hyperlink for each cell in the target range. Logs the time it takes to complete traversal.
+ * @param targetRange Target range to clear the hyperlinks from.
+ */
+function removeHyperLink(targetRange: ExcelScript.Range): void {
+  const rowCount = targetRange.getRowCount();
+  const colCount = targetRange.getColumnCount();
+  console.log(`Searching for hyperliinks in ${targetRange.getAddress()} which contains ${(rowCount * colCount)} cells`);
+  let clearedCount = 0;
+  let cellsVisited = 0;
 
-  const startTime = new Date().getTime();
-  console.log(`Cells per chunk setting: ${cellsInChunk}`);
-  if (!values) {
-    console.log(`Invalid input values to update.`);
-    return false;
-  }
-  if (values.length === 0 || values[0].length === 0) {
-    console.log(`Empty data -- nothing to update.`);
-    return true;
-  }
-  const totalCells = values.length * values[0].length;
-
-  console.log(`Total cells to update in the target range: ${totalCells}`);
-  if (totalCells <= cellsInChunk) {
-    console.log(`No need to chunk -- updating directly`);
-    updateTargetRange(startCell, values);
-    return true;
-  }
-
-  const rowsPerChunk = Math.floor(cellsInChunk / values[0].length);
-  console.log("Rows per chunk " + rowsPerChunk);
-  let rowCount = 0;
-  let totalRowsUpdated = 0;
-  let chunkCount = 0;
-
-  for (let i = 0; i < values.length; i++) {
-    rowCount++;
-    if (rowCount === rowsPerChunk) {
-      chunkCount++;
-      console.log(`Calling update next chunk function. Chunk#: ${chunkCount}`);
-      updateNextChunk(startCell, values, rowsPerChunk, totalRowsUpdated);
-      rowCount = 0;
-      totalRowsUpdated += rowsPerChunk;
-      console.log(`${((totalRowsUpdated / values.length) * 100).toFixed(1)}% Done`);
-
+  let groupStart = new Date().getTime();
+  for (let i = 0; i < rowCount; i++) {
+    for (let j = 0; j < colCount; j++) {
+      cellsVisited++;
+      if (cellsVisited % 50 === 0) {
+        let groupEnd = new Date().getTime();
+        console.log(`Completed ${cellsVisited} cells out of ${rowCount * colCount}. This group took: ${(groupEnd - groupStart) / 1000} seconds to complete.`);
+        groupStart = new Date().getTime();
+      }
+      const cell = targetRange.getCell(i, j);
+      const hyperlink = cell.getHyperlink();
+      if (hyperlink) {
+        cell.clear(ExcelScript.ClearApplyTo.hyperlinks);
+        cell.getFormat().getFont().setUnderline(ExcelScript.RangeUnderlineStyle.none);
+        cell.getFormat().getFont().setColor('Black');
+        clearedCount++;
+      }
     }
   }
-  console.log(`Updating remaining rows -- last chunk: ${rowCount}`)
-  if (rowCount > 0) {
-    updateNextChunk(startCell, values, rowCount, totalRowsUpdated);
-  }
-
-  let endTime = new Date().getTime();
-  console.log(`Completed ${totalCells} cells update. It took: ${((endTime - startTime) / 1000).toFixed(6)} seconds to complete. ${((((endTime  - startTime) / 1000)) / cellsInChunk).toFixed(8)} seconds per ${cellsInChunk} cells-chunk.`);
-
-  return true;
-}
-
-/**
- * A Helper function that computes the target range and updates. 
- */
-
-function updateNextChunk(
-  startingCell: ExcelScript.Range,
-  data: (string | boolean | number)[][],
-  rowsPerChunk: number,
-  totalRowsUpdated: number
-) {
-
-  const newStartCell = startingCell.getOffsetRange(totalRowsUpdated, 0);
-  const targetRange = newStartCell.getResizedRange(rowsPerChunk - 1, data[0].length - 1);
-  console.log(`Updating chunk at range ${targetRange.getAddress()}`);
-  const dataToUpdate = data.slice(totalRowsUpdated, totalRowsUpdated + rowsPerChunk);
-  try {
-    targetRange.setValues(dataToUpdate);
-  } catch (e) {
-    throw `Error while updating the chunk range: ${JSON.stringify(e)}`;
-  }
+  console.log(`Done. Inspected ${cellsVisited} cells. Clearned hyperlinks in: ${clearedCount} cells`);
   return;
-}
-
-/**
- * A Helper function that computes the target range given the target range's starting cell and selected range and updates the values. 
- */
-function updateTargetRange(
-  targetCell: ExcelScript.Range,
-  values: (string | boolean | number)[][]
-) {
-  const targetRange = targetCell.getResizedRange(values.length - 1, values[0].length - 1);
-  console.log(`Updating the range. ${targetRange.getAddress()}`);
-  try {
-    targetRange.setValues(values);
-  } catch (e) {
-    throw `Error while updating the whole range: ${JSON.stringify(e)}`;
-  }
-  return;
-}
-
-// Credit: https://www.codegrepper.com/code-examples/javascript/random+text+generator+javascript
-function getRandomString(length: number): string {
-  var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var result = '';
-  for (var i = 0; i < length; i++) {
-    result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
-  }
-  return result;
 }
