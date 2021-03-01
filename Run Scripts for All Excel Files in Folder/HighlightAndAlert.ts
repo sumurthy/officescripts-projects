16a4ccd649d1253cd69e37c3932f8f875d95f185
Sudhi Ramamurthy <suramam@microsoft.com>
2021-03-01 06:31:37 -0800

    Create HighlightAndAlert.ts

---
+++ Run Scripts for All Excel Files in Folder/HighlightAndAlert.ts
@@ -0,0 +1,39 @@
+function main(workbook: ExcelScript.Workbook) {
+  let table1 = workbook.getTable("Table1");
+  const rowCount = table1.getRowCount();
+  if (rowCount === 0) {
+    return;
+  }
+  workbook.getApplication().calculate(ExcelScript.CalculationType.full);
+
+  const amountDueCol = table1.getColumnByName('Amount Due');
+  const amountDueValues = amountDueCol.getRangeBetweenHeaderAndTotal().getValues();
+
+  let highestValue = amountDueValues[0][0];
+  let row = 0;
+  for (let i = 1; i < amountDueValues.length; i++) {
+    if (amountDueValues[i][0] > highestValue) {
+      highestValue = amountDueValues[i][0];
+      row = i;
+    }
+  }
+  // Set fill color to FFFF00 for range in table Table1 cell in row 0 on column Amount due
+  table1.getColumn("Amount due")
+    .getRangeBetweenHeaderAndTotal()
+    .getRow(row)
+    .getFormat()
+    .getFill()
+    .setColor("FFFF00");
+  let selectedSheet = workbook.getActiveWorksheet();
+  // Insert comment at cell InvoiceAmounts!F2
+  workbook.addComment(table1.getColumn("Amount due")
+    .getRangeBetweenHeaderAndTotal()
+    .getRow(row), {
+    mentions: [{
+      email: "AdeleV@M365x904181.OnMicrosoft.com",
+      id: 0,
+      name: "Adele Vance"
+    }],
+    richContent: "<at id=\"0\">Adele Vance</at> Please review this amount"
+  }, ExcelScript.ContentType.mention);
+}
