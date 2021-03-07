959fea91ef5eaf805229b63c1a4e75196b06bb29
Sudhi Ramamurthy <suramam@microsoft.com>
2021-03-06 17:46:46 -0800

    Create Top4Tips.ts

---
+++ Top 5 Tips/Top4Tips.ts
@@ -0,0 +1,36 @@
+function main(workbook: ExcelScript.Workbook) {
+
+  const xlDate1value = workbook.getActiveWorksheet().getRange('A2').getValue();
+  const xlDate2value = workbook.getActiveWorksheet().getRange('B2').getValue();
+
+  workbook.getActiveCell().clear(ExcelScript.ClearApplyTo.contents);
+
+  const jsDate1 = xlDateToJSDate(xlDate1value as number);
+  console.log(jsDate1.toUTCString())
+
+  const jsDate2 = xlDateToJSDate(xlDate2value as number);
+  console.log(jsDate2.toUTCString())
+  
+  // Search for Breaks in the current worksheet 
+  const result = workbook.getActiveWorksheet().findAll("brakes", {matchCase: false});
+  if (result) {
+    console.log(result.getAddress());
+
+    // Split the resulting adddress string into array 
+    const cellAddressArray = result.getAddress().split(',');
+    console.log(cellAddressArray);
+  }
+}
+
+
+/**
+ * Function to return the JS date from Excel date
+ * 
+ */
+function xlDateToJSDate(serialDate: number): Date {
+  var days = Math.floor(serialDate);
+  var hours = Math.floor((serialDate % 1) * 24);
+  var minutes = Math.floor((((serialDate % 1) * 24) - hours) * 60)
+  const returnDate = new Date(Date.UTC(0, 0, serialDate, hours - 17, minutes));
+  return returnDate;
+}
