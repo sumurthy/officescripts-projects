6d1d11653f9a490c15db1ea8b4db66958590bd3c
Sudhi Ramamurthy <suramam@microsoft.com>
2021-03-10 18:15:07 -0800

    Create CountBlankRowsForAllSheets.ts

---
+++ Count Blank Rows/CountBlankRowsForAllSheets.ts
@@ -0,0 +1,26 @@
+function main(workbook: ExcelScript.Workbook): number
+{
+  const sheets = workbook.getWorksheets();
+  let emptyRows = 0;
+  for (let sheet of sheets) { 
+    const range = sheet.getUsedRange(true); // get value only 
+    if (!range) {
+      console.log(`No data on this sheet. `);
+      continue;
+    }
+    console.log(`Used range for the worksheet ${sheet.getName()}: ${range.getAddress()}`);
+    const values = range.getValues();
+
+    for (let row of values) {
+      let len = 0;
+      for (let cell of row) {
+        len = len + cell.toString().length;
+      }
+      if (len === 0) {
+        emptyRows++;
+      }
+    }
+  }
+  console.log(`Total empty row: ` + emptyRows);
+  return emptyRows;
+}
